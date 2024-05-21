9615818e2f6646faee6aa37f61b35e5ebc1a40db
vansangpfiev <sang@jan.ai>
2024-05-21 14:57:38 +0700

    fix: build name

--- base/cortex-common/enginei.h
+++ base/cortex-common/enginei.h
@@ -9,12 +9,18 @@ class EngineI {
  public:
   virtual ~EngineI() {}
 
-  virtual void ExecutePythonFile(
-    std::string binary_execute_path,
-    std::string file_execution_path,
-    std::string python_library_path) = 0;
+  virtual void ExecutePythonFile(std::string binary_execute_path,
+                                 std::string file_execution_path,
+                                 std::string python_library_path) = 0;
 
   virtual void HandlePythonFileExecutionRequest(
-    std::shared_ptr<Json::Value> json_body,
-    std::function<void(Json::Value&&, Json::Value&&)>&& callback) = 0;
+      std::shared_ptr<Json::Value> json_body,
+      std::function<void(Json::Value&&, Json::Value&&)>&& callback) = 0;
+
+  virtual bool IsSupported(const std::string& f) {
+    if (f == "ExecutePythonFile" || f == "HandlePythonFileExecutionRequest") {
+      return true;
+    }
+    return false;
+  }
 };
