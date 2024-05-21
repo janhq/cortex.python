d7b2063eb924cfbf5c60492a71cbcb66d28c8f32
vansangpfiev <vansangpfiev@gmail.com>
2024-05-21 17:56:27 +0700

    fix: change file path, makefile, engine interface (#13)
    * fix: change file path, makefile, engine interface
    
    * fix: e2e
    
    * fix: e2e 2
    
    ---------
    
    Co-authored-by: vansangpfiev <sang@jan.ai>

--- base/cortex-common/enginei.h
+++ base/cortex-common/enginei.h
@@ -9,6 +9,22 @@ class EngineI {
  public:
   virtual ~EngineI() {}
 
+  virtual void HandleChatCompletion(
+      std::shared_ptr<Json::Value> json_body,
+      std::function<void(Json::Value&&, Json::Value&&)>&& callback) {}
+  virtual void HandleEmbedding(
+      std::shared_ptr<Json::Value> json_body,
+      std::function<void(Json::Value&&, Json::Value&&)>&& callback) {}
+  virtual void LoadModel(
+      std::shared_ptr<Json::Value> json_body,
+      std::function<void(Json::Value&&, Json::Value&&)>&& callback)  {}
+  virtual void UnloadModel(
+      std::shared_ptr<Json::Value> json_body,
+      std::function<void(Json::Value&&, Json::Value&&)>&& callback)  {}
+  virtual void GetModelStatus(
+      std::shared_ptr<Json::Value> json_body,
+      std::function<void(Json::Value&&, Json::Value&&)>&& callback)  {}
+
   virtual void ExecutePythonFile(std::string binary_execute_path,
                                  std::string file_execution_path,
                                  std::string python_library_path) = 0;
